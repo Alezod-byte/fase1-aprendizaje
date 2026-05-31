22cc545c3de2399cc83fca38c8c1eccc5a44a3c8
alezod <ale.techprog@gmail.com>
2026-05-31 20:20:31 -0300

    Agrega variables.py con tipos básicos

---
+++ variables.py
@@ -0,0 +1,13 @@
+nombre = "alezod"
+edad = 20
+altura = 1.75
+
+print(nombre)
+print(edad)
+print(altura)
+print(type(nombre))
+print(type(edad))
+print(type(altura))
+es_artista = True
+print(es_artista)
+print(type(es_artista))
